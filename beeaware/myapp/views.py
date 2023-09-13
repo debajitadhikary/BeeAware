f04ec14da110f80029b6b24d23ac0bea07b8efe0
Debajit Adhikary <95972669+debajitadhikary@users.noreply.github.com>
2023-09-13 23:21:08 +0530

    migration established

--- beeaware/myapp/views.py
+++ beeaware/myapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Feature
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    features = Feature.objects.all()
+    return render(request,'index.html', {'features':features})
     # return HttpResponse('<h1> Het , Welcome<h1>')
