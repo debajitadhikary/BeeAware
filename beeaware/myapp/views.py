9065e3e210f04006b12ec8af1bde20c3c959f009
Debajit Adhikary <95972669+debajitadhikary@users.noreply.github.com>
2023-09-14 00:39:22 +0530

    updation

--- beeaware/myapp/views.py
+++ beeaware/myapp/views.py
@@ -35,6 +35,18 @@ def register(request):
         return render(request , 'register.html')
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username , password = password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
 
 
