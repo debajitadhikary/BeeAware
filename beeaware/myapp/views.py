7bcaef42f8adb04719c02eafdf570730f11783cb
Debajit Adhikary <95972669+debajitadhikary@users.noreply.github.com>
2023-09-13 21:33:03 +0530

    updation

--- beeaware/myapp/views.py
+++ beeaware/myapp/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request,'index,html')
+    return render(request,'index.html')
     # return HttpResponse('<h1> Het , Welcome<h1>')
